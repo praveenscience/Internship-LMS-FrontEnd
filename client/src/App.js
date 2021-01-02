4c29f9d1ab120ae11817cf10ba9fab21a3e7df40
Praveen Kumar <praveenscience@gmail.com>
2021-01-02 11:54:49 +0000

    Created a React JS BoilerPlate using CRA.

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello Winter of Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
