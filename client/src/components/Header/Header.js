fe4d986f436f34adb49e8d02062c95bc80349bad
NEHA PARMAR <nehaparmar@NEHAs-MacBook-Pro.local>
2021-01-29 02:42:03 +0530

    formatted code

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,42 +1,42 @@
-import React, {useEffect } from 'react'
-import {FaApple} from "react-icons/fa";
-import {AiOutlineSearch } from "react-icons/ai";
- import { IoBagOutline } from "react-icons/io5";
- import "../../styles/Header/Header.scss"
+import React, { useEffect } from "react";
+import { FaApple } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoBagOutline } from "react-icons/io5";
+import "../../styles/Header/Header.scss";
 function Header() {
   useEffect(() => {
     const navIcon = document.querySelector(".nav-icon");
     const nav = document.querySelector("nav");
     navIcon.onclick = function () {
-        nav.classList.toggle('show');
-    }
-  }, [])
-    return (
-      <nav> 
+      nav.classList.toggle("show");
+    };
+  }, []);
+  return (
+    <nav>
       <div className="nav-content">
-          <FaApple size={25} color=" white" className="icon"/>
-          
-          <div className="nav-icon">
-              <div className="bar one"></div>
-              <div className="bar two"></div>
-          </div>
+        <FaApple size={25} color=" white" className="icon" />
 
-          <div className="nav-links">
-              <a href="#">Mac</a>
-              <a href="#">iPad</a>
-              <hr className="hra"></hr>
-              <a href="#">iPhone</a>
-              <a href="#">Watch</a>
-              <a href="#">TV</a>
-              <a href="#">Music</a>
-              <a href="#">Support</a>
-          </div>
+        <div className="nav-icon">
+          <div className="bar one"></div>
+          <div className="bar two"></div>
+        </div>
 
-          <AiOutlineSearch size={23} color="white" className="search" />
+        <div className="nav-links">
+          <a href="#">Mac</a>
+          <a href="#">iPad</a>
+          <hr className="hra"></hr>
+          <a href="#">iPhone</a>
+          <a href="#">Watch</a>
+          <a href="#">TV</a>
+          <a href="#">Music</a>
+          <a href="#">Support</a>
+        </div>
+
+        <AiOutlineSearch size={23} color="white" className="search" />
         <IoBagOutline color="white" size={23} className="search1" />
       </div>
-  </nav>
-    )
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
