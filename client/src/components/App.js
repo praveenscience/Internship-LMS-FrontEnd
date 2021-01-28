15d88a71eef04023ac4bd8d83138097b2a50e6b3
NEHA PARMAR <nehaparmar@NEHAs-MacBook-Pro.local>
2021-01-29 02:43:45 +0530

    formatted code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
         {/* <p>Hello Winter of Code</p> */}
       </div>
     );
