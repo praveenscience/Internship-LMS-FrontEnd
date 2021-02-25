e037fe064740722cb94031623db7165514672774
nehaparmar99 <48972593+nehaparmar99@users.noreply.github.com>
2021-02-25 20:33:48 +0530

    Merging Header Temp. (#17)
    * Header added
    * formatted code
    * formatted code
    * changes made
    * changes made
    * changes made
    
    Co-authored-by: NEHA PARMAR <nehaparmar@NEHAs-MacBook-Pro.local>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import Header from "./Header/Header"
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello Winter of Code</p>
+        <Header />
+        {/* <p>Hello Winter of Code</p> */}
       </div>
     );
   }
