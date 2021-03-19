01424de9e62c404aa55aa93e0568bb1dbdd296e8
Praveen Kumar <praveenscience@gmail.com>
2021-03-19 10:35:49 +0000

    Imported the styles in the main script.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
+import "./styles/index.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
