609724017d7b83f889acec6b996174fc1776c39a
Praveen Kumar <praveenscience@gmail.com>
2021-04-01 17:30:05 +0100

    Checking LintStaged.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true,
+        "es2021": true,
+        "jest": true
     },
     "extends": [
         "eslint:recommended",
@@ -17,7 +19,7 @@ module.exports = {
     "plugins": [
         "react"
     ],
-    "ignorePatterns": ["src/tests/**/*.js"],
+    "ignorePatterns": [],
     "rules": {
         "indent": [
             "error",
@@ -30,6 +32,30 @@ module.exports = {
         "semi": [
             "error",
             "always"
+        ]
+    },
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+                                                // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+            "version": "detect", // React version. "detect" automatically picks the version you have installed.
+                                // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                                // default to latest and warns if missing
+                                // It will default to "detect" in the future
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": [
+            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+            "forbidExtraProps",
+            {"property": "freeze", "object": "Object"},
+            {"property": "myFavoriteWrapper"}
+        ],
+        "linkComponents": [
+            // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+            "Hyperlink",
+            {"name": "Link", "linkAttribute": "to"}
         ]
     }
 };
--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-export const linters = {
-  "**/*.+(js|md|ts|css|sass|less|yml|yaml|scss|json|jsx)": [
-    "eslint --fix",
-    "prettier --write",
-    "git add"
-  ]
-};
